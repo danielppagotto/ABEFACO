d7160e9be893f003ddf66bcbf06aba468f974983
admhenri <84077982+AdalenyPaiva@users.noreply.github.com>
2025-03-17 14:34:01 -0300

    Create trat_pop_faixa_etaria_sexo.R

---
+++ 01_script/trat_pop_faixa_etaria_sexo.R
@@ -0,0 +1,51 @@
+library(tidyverse)
+library(readxl)
+
+# carregando as tabelas necessarias 
+
+tabela_rj_m <- read_excel("01_script/00_bases/tabela_rj_m.xlsx")
+tabela_rj_h <- read_excel("01_script/00_bases/tabela_rj_h.xlsx")
+
+
+#### Tratando os dados 
+
+#-------------- base dados mulheres
+
+#convertendo as colunas das faixas etarias para o mesmo tipo
+pop_rj_m <- tabela_rj_m %>%
+  mutate(across(
+    -c(cod_ibge, municipio, sexo),  
+    as.numeric                      
+  ))
+  
+# tranformando colunas em linhas 
+pop_rj_m_pivot <- pop_rj_m %>%
+  pivot_longer(
+    cols = -c(cod_ibge, municipio,sexo),     
+    names_to = "faixa_etaria",       
+    values_to = "valor")
+
+
+#---------------- base dados homens
+
+#convertendo as colunas das faixas etarias para o mesmo tipo
+pop_rj_h <- tabela_rj_h %>%
+  mutate(across(
+    -c(cod_ibge, municipio, sexo),  
+    as.numeric                      
+  ))
+
+#tranformando colunas em linhas
+pop_rj_h_pivot <- pop_rj_h %>%
+  pivot_longer(
+    cols = -c(cod_ibge, municipio,sexo),     
+    names_to = "faixa_etaria",       
+    values_to = "valor")
+
+
+#------------------------------ Juntando as bases de homens e mulheres 
+  
+
+pop_rj <- bind_rows(pop_rj_h_pivot, pop_rj_m_pivot)
+
+
