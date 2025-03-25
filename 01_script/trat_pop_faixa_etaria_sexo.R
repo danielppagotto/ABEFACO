93f39190f4ab51ab515b78420fcd878534b69894
admhenri <84077982+AdalenyPaiva@users.noreply.github.com>
2025-03-25 11:16:05 -0300

    Update trat_pop_faixa_etaria_sexo.R

--- 01_script/trat_pop_faixa_etaria_sexo.R
+++ 01_script/trat_pop_faixa_etaria_sexo.R
@@ -1,7 +1,11 @@
+
+
 library(tidyverse)
 library(readxl)
 
-# carregando as tabelas necessarias 
+
+
+# carregando as tabelas necessarias com a pop pro faixa etaria de homem e a outra de mulher 
 
 tabela_rj_m <- read_excel("01_script/00_bases/tabela_rj_m.xlsx")
 tabela_rj_h <- read_excel("01_script/00_bases/tabela_rj_h.xlsx")
@@ -50,25 +54,34 @@ pop_rj_h_pivot <- pop_rj_h %>%
   
 
 pop_rj <- bind_rows(pop_rj_h_pivot, pop_rj_m_pivot)
-pop_rj$municipio <- gsub(" \\(RJ\\)", "", pop_rj$municipio)
+pop_rj$municipio <- gsub(" \\(RJ\\)", "", pop_rj$municipio) #retirando (RJ) do nome dos municipios
 pop_rj <- pop_rj %>%
-  mutate(cod_ibge = str_sub(cod_ibge, 1, -2))
+  mutate(cod_ibge = str_sub(cod_ibge, 1, -2)) #excluindo o digito verificador para ser compativel com a consulta do data_lake
 
+#baixando o dataframe 
 writexl::write_xlsx(pop_rj,"pop_rj_tratado.xlsx")
 
 
-# Filtrando as faixas etarias 
+# pop_rj = dataframe que tem todos o municipios do RJ com a pop por faixa etaria e por sexo ja com as colunas empilhadas
+
+#--------------------------- # Filtrando as faixas etarias 
 
 
+#colocando as coluna de faixa etaria no tipo character para poder filtrar
 pop_rj <- pop_rj%>%
   mutate(across(
     -c(cod_ibge, municipio, sexo,valor),  
     as.character                     
   ))
 
+#renomenando a coluna valor para populacao
 pop_rj <- pop_rj%>%
     rename(populacao = valor)
 
+
+#colocando as faixas etaria acima de 60 anos no intervalo de 60 ou mais
+
+#pop_rj_filtrado = dataframe que tem apenas as faixas etarias de acordo com a planilha de vacinacao
 
 pop_rj_filtrado <- pop_rj %>%  
   mutate(faixa_etaria = case_when(
@@ -82,26 +95,35 @@ pop_rj_filtrado <- pop_rj %>%
                         "95 anos", "96 anos", "97 anos", "98 anos", "99 anos",
                         "100 anos ou mais") ~ "60 anos ou mais",
     TRUE ~ faixa_etaria  
-  ))%>% 
+  ))%>% #filtrando faixa etarias de acordo com a tabela de vacinacao
   group_by(cod_ibge, municipio, sexo, faixa_etaria) %>%  
   summarise(populacao = sum(populacao, na.rm = TRUE), .groups = "drop")%>%
   filter(faixa_etaria %in% c("Menos de 1 mês", "2 meses","3 meses","4 meses",
                              "5 meses","6 meses","7 meses","9 meses", "1 ano",
                              "4 anos","7 anos","9 anos","10 anos",
-                             "11 anos","12 anos","13 anos", "14 anos"))
+                             "11 anos","12 anos","13 anos", "14 anos")) 
       
-#--------------------- Oferta
+#--------------------- dataframe do datalake
 
-
+#Transformando a linhas da coluna de profissionais em duas colunas
 oferta_prof_pivot <- oferta_prof %>%
   pivot_wider(
-    names_from = prof_recod,       
-    values_from = c(CH, fte40))
+    names_from = prof_recod, #onde estao os dados que vão virar o nome das colunas     
+    values_from = c(CH, fte40)) # os dados que vao para essa novas colunas
     
-  
-
+#renomenado a coluna do tec/auxiliar pq estava com espaco
 oferta_prof_pivot <- oferta_prof_pivot %>%
   rename(fte40_tec_aux_enfermagem = `fte40_Técnico/auxiliar de enfermagem`,CH_tec_aux_enfermagem = `CH_Técnico/auxiliar de enfermagem` )
+
+
+
+#----------------------- Preparando o mapa do RJ com o shapefile baixado do ibge
+
+RJ <- st_read("01_script/00_bases/RJ_Municipios_2023/RJ_Municipios_2023.shp")
+
+#retirando o digito verificador para poder fazer o leftjoin
+RJ2 <- RJ %>%
+  mutate(CD_MUN = str_sub(CD_MUN, 1, -2))
 
 
 
